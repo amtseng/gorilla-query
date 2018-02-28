a9b3225bf1a85f5bc4891adcf8228a9e946e477b
Alex Tseng <atseng95@gmail.com>
2018-02-28 15:38:01 -0800

    Working system

---
+++ README.md
@@ -0,0 +1,33 @@
+### GOrilla Query
+#### Background
+Despite the awful acronym (if it can even be called one), [GOrilla](http://cbl-gorilla.cs.technion.ac.il/) is an excellent tool to discover GO term enrichments in a ranking of genes. Unfortunately, the tool is only available (to my knowledge) through a web interface, so I wrote this small script to query GOrilla programmatically, from commandline. This script allows the user to submit an enrichment query for a single target ranking of genes, for _H. sapiens_, and then scrapes down the results for GO Biological Process, Function, and Component.
+Other functionalities to take advantage of other GOrilla usages can be easily extended from this script.
+
+#### Usage
+`python gorilla_query.py -g [GENE_LIST] -o [OUTPUT_DIR]`
+- `GENE_LIST`
+	- A file containing a ranking of `\n`-delimited genes
+		- Genes are preferably given as official gene symbols, but Ensembl IDs are also acceptable (see [the web interface](http://cbl-gorilla.cs.technion.ac.il/) for more details
+		- `OUTPUT_DIR`
+			- GOrilla Query outputs three files into this directory (which is created if it does not exist already), corresponding to the gene enrichment tables for Process, Function, and Component
+				- Files will be `OUTPUT_DIR/process.tsv`, `OUTPUT_DIR/function.tsv`, and `OUTPUT_DIR/component.tsv`
+
+#### Output
+Each output table consists of the following:
+**Line 1:** A stable web link (for about 1 month) to the results on GOrilla's servers (this link also contains more information about results)
+**Line 2:** The total number of genes tested by GOrilla, $N$
+**Line 3:** Header of the results table
+**Lines 4-n:** Rows of the results table, in decreasing order of significance
+The following is a description of each column:
+- `term`: The GO-term ID (e.g. `GO0051302`)
+- `desc`: The description of the GO-term (e.g. `regulation of cell division`)
+- `pval`: p-value of the term enrichment, uncorrected for multiple hypothesis testing
+- `fdr`: FDR of the term enrichment, corrected for multiple hypothesis testing
+- `enrichment`: Enrichment value, calculated as $\frac{b}{n}/\frac{B}{N}$
+- `B`: The total number of genes associated with this term, $B$
+- `n`: The number of genes that appear at the top of the ranking (see the citation below for more details), $n$
+- `b`: The intersection of genes that appear at the top of the ranking and the genes associated with the term, $b$
+
+#### Citations
+Eran Eden*, Roy Navon*, Israel Steinfeld, Doron Lipson and Zohar Yakhini. "**GOrilla: A Tool For Discovery And Visualization of Enriched GO Terms in Ranked Gene Lists**", [BMC Bioinformatics 2009, 10:48](http://www.biomedcentral.com/1471-2105/10/48).
+
---
+++ gorilla_query.py
@@ -0,0 +1,120 @@
+import sys
+import os
+import argparse
+import mechanize
+import urlparse
+from bs4 import BeautifulSoup
+
+GORILLA_URL = "http://cbl-gorilla.cs.technion.ac.il/"
+GO_ONTOS = {"process": "GOResultsPROCESS.html", "function": "GOResultsFUNCTION.html", "component": "GOResultsCOMPONENT.html"}
+
+def parse_arguments():
+	"""
+	Parses arguments and returns a dictionary of the arguments.
+	"""
+	parser = argparse.ArgumentParser(description="Submits genes to GOrilla for term enrichment analysis")
+	parser.add_argument("-g", "--genefile", type=str, required=True, help="Path to a file containing newline-delimited genes, preferably as gene symbols")
+	parser.add_argument("-o", "--outdir", type=str, required=True, help="Directory to output the enrichment tables")
+	
+	args = parser.parse_args()
+
+	genefile, outdir = args.genefile, args.outdir
+	with open(genefile, "r") as f:
+		gene_list = [line.strip() for line in f]
+	
+	return {
+		"g": gene_list,
+		"o": outdir
+	}
+
+
+def submit_gorilla(gene_list):
+	"""
+	Sends the gene list to GOrilla and returns the base URL for the results.
+	"""
+	br = mechanize.Browser()
+	br.set_handle_redirect(mechanize.HTTPRedirectHandler)
+	
+	br.open(GORILLA_URL)
+	br.select_form(nr=0)
+	br.form["target_set"] = "\n".join(gene_list)
+	br.form["db"] = ["all"]
+	
+	br.submit()
+
+	url = str(br.geturl())
+	url_tokens = url.split("/")
+	return "/".join(url_tokens[:-1])
+
+
+def scrape_gorilla(base_url, page_name):
+	"""
+	Scrapes the results page given by the base URL and the specific page name
+	(e.g. GOResultsPROCESS.html). The results are scraped down as a list of
+	tuples:
+		term ID, description, p val, FDR, enrichment, N, B, n, b, genes	
+	Returns the URL, total number of genes tested, and this list of tuples.
+	"""
+	url = "/".join([base_url, page_name])
+
+	br = mechanize.Browser()
+	br.set_handle_redirect(mechanize.HTTPRedirectHandler)
+	br.open(url)
+	soup = BeautifulSoup(br.response().read(), "lxml")
+
+	if str(soup.title.text) == "No GO Enrichment Found":
+		return url, 0, []
+
+	results_table = soup.find_all("table")[1]  # Second table on the page
+	
+	table_entries = []  # GO term, description, p, FDR, enrichment, B, n, b, genes
+	N = ""
+	for row in results_table.find_all("tr")[1:]:  # First row is header
+		row_data = [entry.text for entry in row.find_all("td")]
+		term, desc, pval, fdr = [str(entry) for entry in row_data[:4]]
+		
+		enrich_data = str(row_data[4]).split()
+		enrich = enrich_data[0]
+		counts = enrich_data[1][1:-1].split(",")
+		N = counts[0]
+			
+		gene_pairs = str(row_data[5]).strip().split("\r\n")[1:]  # First entry is '[+] Show genes'
+		gene_pairs = [pair.split("-") for pair in gene_pairs]
+		genes = {pair[0].strip() : pair[1].strip() for pair in gene_pairs}
+		table_entries.append((term, desc, pval, fdr, enrich, counts[1], counts[2], counts[3], genes))
+
+	return url, N, table_entries
+
+
+def write_table(url, total_genes, table_entries, outpath):
+	"""
+	Writes the table entries returned by `scrape_gorilla` to the path given.
+	"""
+	with open(outpath, "w") as outfile:
+		header = "\t".join(["term", "desc", "pval", "fdr", "enrichment", "B", "n", "b", "genes"])
+		outfile.write("# URL: {0}\n".format(url))
+		outfile.write("# Total genes: {0}\n".format(total_genes))
+		outfile.write(header + "\n")
+
+		for row in table_entries:
+			row_data = list(row[:-1])
+			row_data += [",".join(row[-1].keys())]  # Only write gene names
+			outfile.write("\t".join(row_data) + "\n")
+
+
+if __name__ == "__main__":
+	arg_dict = parse_arguments()
+	gene_list = arg_dict["g"]
+	outdir = arg_dict["o"]
+
+	if not os.path.isdir(outdir):
+		os.mkdir(outdir)
+
+	print("Submitting to GOrilla...")	
+	base_url = submit_gorilla(gene_list)
+
+	for onto_type in GO_ONTOS:
+		print("Scraping results for GO {0}...".format(onto_type.upper()))
+		url, N, table = scrape_gorilla(base_url, GO_ONTOS[onto_type])
+		outpath = os.path.join(outdir, "{0}.tsv".format(onto_type))
+		write_table(url, N, table, outpath)
